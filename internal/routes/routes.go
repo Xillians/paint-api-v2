e703e5189e2ec4b046a578d16118a26e209caaba
Xillians <sandra.toftemo@bjerk.io>
2025-01-08 21:37:35 +0100

    add empty register routes

---
+++ internal/routes/routes.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/danielgtaylor/huma/v2"
+	"gorm.io/gorm"
+)
+
+// RegisterRoutes registers the routes for the API
+func RegisterRoutes(api huma.API, db *gorm.DB) {
+}
