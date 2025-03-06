e4e3a2cb286c817052ac73f6a33fe9fbbe05033c
Xillians <sandra.toftemo@bjerk.io>
2025-03-06 13:53:18 +0100

    add userId to context for request

--- internal/middleware/jwt.go
+++ internal/middleware/jwt.go
@@ -1,9 +1,12 @@
 package middleware
 
 import (
+	"log/slog"
 	"paint-api/internal/jwt"
 	"regexp"
 	"strings"
+
+	j "github.com/golang-jwt/jwt/v5"
 
 	"github.com/danielgtaylor/huma/v2"
 )
@@ -26,12 +29,20 @@ func AuthenticateRequests(api huma.API, jwtService jwt.JWTService) func(huma.Con
 		}
 
 		authHeader := ctx.Header("Authorization")
-		jwt := strings.Split(authHeader, " ")[1]
-		_, err := jwtService.VerifyToken(jwt)
+		jwtToken := strings.Split(authHeader, " ")[1]
+		token, err := jwtService.VerifyToken(jwtToken)
 		if err != nil {
 			huma.WriteErr(api, ctx, 401, "Unauthorized", err)
 			return
 		}
+
+		userId := token.Claims.(j.MapClaims)["user_id"]
+		if userId == nil {
+			huma.WriteErr(api, ctx, 401, "Unauthorized: User ID not found in token", nil)
+			return
+		}
+		slog.Info("User ID", "userId", userId)
+		ctx = huma.WithValue(ctx, "userId", userId)
 
 		next(ctx)
 	}
